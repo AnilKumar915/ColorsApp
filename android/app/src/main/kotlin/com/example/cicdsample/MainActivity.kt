cac678927b2c38e3928397a58b6d1beb813a218e
Anil Kumar <anil.kumar@ostrumtech.com>
2023-03-17 11:43:38 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/example/cicdsample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cicdsample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ScreenColors.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ScreenColors extends StatefulWidget {
+  const ScreenColors({super.key});
+
+  @override
+  State<ScreenColors> createState() => _ScreenColorsState();
+}
+
+int colorValue = 0;
+
+class _ScreenColorsState extends State<ScreenColors> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: colorValue == 0 ? Colors.red : Colors.green,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: (() {
+                setState(() {
+                  colorValue = 0;  
+                });
+                
+              }), child: const Text("Red")),
+              ElevatedButton(onPressed: (() {
+                setState(() {
+                  colorValue = 1;  
+                });
+              }), child: const Text("Green"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
